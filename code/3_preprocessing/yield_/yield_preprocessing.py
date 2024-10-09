9639c8a18fb4885a113226ce4c486c81bfa18c81
Philip Janz <philip.janz.arbeit@gmail.com>
2024-10-09 22:52:34 +0200

    yield standardization with linear kernel

--- code/3_preprocessing/yield_/yield_preprocessing.py
+++ code/3_preprocessing/yield_/yield_preprocessing.py
@@ -1,4 +1,6 @@
 import numpy as np
+import statsmodels.api as sm
+
 from sklearn.linear_model import LinearRegression
 
 from yield_.yield_functions import *
@@ -24,8 +26,8 @@ comb_df = comb_df[comb_df.harv_year > 2000]
 comb_df.sort_values(["country", "adm1", "adm2", "harv_year"], inplace=True, ignore_index=True)
 
 # Yield anomalies processing
-comb_df["rel_yield_anomaly"] = np.nan
-comb_df["yield_anomaly"] = np.nan
+comb_df["standardized_yield"] = np.nan
+pvalue_ls = []
 for adm, adm_yield_df in comb_df.groupby(["country", "adm1", "adm2"]):
     # make linear and quadratic fit of yield data to capture trends
     X_lin = adm_yield_df["harv_year"].values.reshape(-1, 1)
@@ -33,28 +35,33 @@ for adm, adm_yield_df in comb_df.groupby(["country", "adm1", "adm2"]):
     model = LinearRegression()
     linear_fit = model.fit(X=X_lin, y=adm_yield_df["yield"].values).predict(X_lin)
     quadratic_fit = model.fit(X=X_quad, y=adm_yield_df["yield"].values).predict(X_quad)
-    n_years = adm_yield_df["harv_year"].nunique()
-    rolling_fit = adm_yield_df["yield"].rolling(window=int(n_years/4), center=False, min_periods=1, win_type='gaussian').mean(std=2)
+    #n_years = adm_yield_df["harv_year"].nunique()
+    #rolling_fit = adm_yield_df["yield"].rolling(window=int(n_years/4), center=False, min_periods=1, win_type='gaussian').mean(std=2)
 
-    # calc anomalies
-    comb_df.loc[adm_yield_df.index, "rel_yield_anomaly"] = adm_yield_df["yield"] / quadratic_fit
-    comb_df.loc[adm_yield_df.index, "yield_anomaly"] = adm_yield_df["yield"] - quadratic_fit
+    # cal p-values
+    pvalue = sm.OLS(adm_yield_df["yield"].values, sm.add_constant(X_lin, prepend=False)).fit().pvalues[0]
+    pvalue_ls.append(pvalue)
+
+
+    # calc standardization
+    detrended_yield = adm_yield_df["yield"] - linear_fit
+    comb_df.loc[adm_yield_df.index, "standardized_yield"] = detrended_yield / np.std(detrended_yield)
 
     # plot
     adm = str(adm).replace(", ", "-").replace("'", "").replace(" None", "")
     plt.plot(adm_yield_df.harv_year, adm_yield_df["yield"],
-             label=f"yield (var = {np.round(np.var(adm_yield_df['yield']), 3)})")
+             label=f"yield")
     plt.plot(adm_yield_df.harv_year, linear_fit,
              label=f"linear fit",
              linestyle="dotted")
     plt.plot(adm_yield_df.harv_year, quadratic_fit,
-             label=f"quadratic fit (anomaly variance = {np.round(np.var(comb_df.loc[adm_yield_df.index, 'yield_anomaly']), 4)})",
+             label=f"quadratic fit",
              linestyle="dotted")
-    plt.plot(adm_yield_df.harv_year, rolling_fit,
-             label=f"rolling avg",
-             linestyle="dotted")
+    #plt.plot(adm_yield_df.harv_year, rolling_fit,
+    #         label=f"rolling avg",
+    #         linestyle="dotted")
     plt.legend()
-    plt.title(f"Benchmarking yield for: {adm}")
+    plt.title(f"Trend in yield time series for: {adm}")
     plt.savefig(PROCESSED_DATA_DIR / f"yield/plots/yield_and_trends_{adm}", dpi=300)
     plt.close()
 
